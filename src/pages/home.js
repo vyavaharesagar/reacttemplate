eaff4fb89a0cbe50f776242c69b57ea478d045fc
sagar <vyavaharesagar303@gmail.com>
2021-06-11 15:24:58 +0530

    -a

---
+++ src/pages/home.js
@@ -0,0 +1,258 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+
+        return (
+
+            <div class="container-fluid px-0 " id="hero">
+                <div class="container-fluid px-0" id="color-overlay">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4">
+                                <a class="d-inline-flex p-2 bd-highlight align-middle" href="#">
+                                    <img src="./img/image 52.png" class="img-fluid" alt="img" />
+                                </a>
+                            </div>
+                            <div class="col-8 d-flex justify-content-end px-0">
+                                <nav class="navbar navbar-expand-lg navbar-dark ">
+                                    <a class="nav-link" href="#">
+                                        <img src="./img/icon/search_white_24dp.svg" class="img-fluid" alt="..." />
+                                    </a>
+                                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                        <ul class="navbar-nav mr-auto">
+                                            <li class="nav-item ">
+                                                <a class="nav-link" href="#">Search</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Help</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Account</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <a class="nav-link" href="#">
+                                        <img src="./img/icon/local_mall_white_24dp.svg" alt="" srcset="" />
+                                    </a>
+                                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                        aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+                                </nav>
+                            </div>
+
+                        </div>
+                        <div class="hero-content">
+                            <h1>Cari Cari</h1>
+                            <p>Live from their sofa to yours. Get closer to your favorite artists, and never miss out.</p>
+                        </div>
+                        <div class="newcard flex-mid container-fluid ">
+                            <div class="pad23 flex-mid">
+                                <div class="circle-card  flex-mid  ">
+                                    <div class="color-circle-card ">
+                                        <div class="flex-mid-col card-content">
+                                            <img class="card-icon" src="img/icon/Like.svg" alt="" />
+                                            <img class="card-icon " src="img/icon/exposure_zero_white_24dp.svg" alt="" />
+                                            <p>Label</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pad23 flex-mid">
+                                <div class="circle-card  flex-mid  ">
+                                    <div class="dummy-color-circle-card ">
+                                        <div class="flex-mid-col card-content">
+                                            <img class="card-icon" src="img/icon/Calendar.svg" alt="" />
+                                            <img class="card-icon " src="img/icon/exposure_zero_white_24dp.svg" alt="" />
+                                            <p>Label</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pad23 flex-mid">
+                                <div class="circle-card  flex-mid  ">
+                                    <div class="dummy-color-circle-card ">
+                                        <div class="flex-mid-col card-content">
+                                            <img class="card-icon" src="img/icon/Calendar.svg" alt="" />
+                                            <img class="card-icon " src="img/icon/exposure_zero_white_24dp.svg" alt="" />
+                                            <p>Label</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pad23 flex-mid">
+                                <div class="circle-card  flex-mid  ">
+                                    <div class="dummy-color-circle-card ">
+                                        <div class="flex-mid-col card-content">
+                                            <img class="card-icon" src="img/icon/Calendar.svg" alt="" />
+                                            <img class="card-icon " src="img/icon/exposure_zero_white_24dp.svg" alt="" />
+                                            <p>Label</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="container-fluid content">
+                    <p id="bn">
+                        <span class="pnd" id="gn">Up</span>
+                        <span class="pnd">comming Shows</span>
+                        <span class="module" id="sp">View All</span>
+                    </p>
+                    <div class="card-grid">
+                        <div class="flex-mid">
+                            <div class="card-holder">
+                                <div class="card-hero">
+                                    <img src="./img/benny_Dayal.png" alt="img" />
+                                    <p class="tag">Folk</p>
+                                    <p class="singer-name">Benny Dayal</p>
+                                    <div class="labels">
+                                        <div class="read-arrow">
+                                            <p class="read-more module">More info</p>
+                                            <img class="arrow" src="./img/icon/Arrow.svg" alt="img" />
+                                        </div>
+                                        <div>
+                                            <img class="ticket module" src="./img/icon/Ticket.svg " alt="img" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex-mid">
+                                <div class="card-holder">
+                                    <div class="card-hero">
+                                        <img src="./img/Vijay_Yesudas.png" alt="img" />
+                                        <p class="tag">Folk</p>
+                                        <p class="singer-name">Vijay Yesudas</p>
+                                        <div class="labels">
+                                            <div class="read-arrow">
+                                                <p class="module read-more">More info</p>
+                                                <img class="arrow" src="./img/icon/Arrow.svg" alt="img" />
+                                            </div>
+                                            <div>
+                                                <img class="ticket module" src="./img/icon/Ticket.svg" alt="img" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex-mid">
+                                <div class="card-holder">
+                                    <div class="card-hero">
+                                        <img src="./img/Andrea Jeremiah.png" alt="img" />
+                                        <p class="tag">Folk</p>
+                                        <p class="singer-name">Andrea Jeremiah</p>
+                                        <div class="labels">
+                                            <div class="read-arrow">
+                                                <p class="module read-more">More info</p>
+                                                <img class="arrow" src="./img/icon/Arrow.svg" alt="img" />
+                                            </div>
+                                            <div>
+                                                <img class="ticket module" src="./img/icon/Ticket.svg" alt="img" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex-mid">
+                                <div class="card-holder">
+                                    <div class="card-hero">
+                                        <img src="./img/Shilpa Rao.png" alt="img" />
+                                        <p class="tag">Folk</p>
+                                        <p class="singer-name">Shilpa Rao</p>
+                                        <div class="labels">
+                                            <div class="read-arrow">
+                                                <p class="read-more module">More info</p>
+                                                <img class="arrow" src="./img/icon/Arrow.svg" alt="img" />
+                                            </div>
+                                            <div>
+                                                <img class="ticket module" src="./img/icon/Ticket.svg" alt="img" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid content pad">
+                    <p class="dd">
+                        <span class="pnd tn">Re</span>
+                        <span class="pnd">views</span>
+                        <span class="cc" >1/12</span>
+                        <img class="module aa" src="./img/icon/Vector.svg" alt="img" />
+                        <img class="module bb" src="./img/icon/Vector (2).svg" alt="img" />
+                    </p>
+                    <div class="main-card-holder">
+                        <div class="second-card-holder">
+                            <div class="second-card-holder-in">
+                                <div class="sec-car-h-s">
+                                    <div>
+                                        <img src="./img/icon/Img (1).svg" alt="img" />
+                                    </div>
+                                    <div class="info">
+                                        <p class="sec-car-name">Hellen Jummy</p>
+                                        <p><img class="flag" src="./img/icon/us.svg" alt="img" /><span class="country">Palo Alto, CA</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="info-content">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae in donec in nisi vitae.
+                                    Vestibulum pellentesque eget
+                                    laoreet adipiscing.
+                        </div>
+                            </div>
+                        </div>
+
+                        <div class="second-card-holder">
+                            <div class="second-card-holder-in">
+                                <div class="sec-car-h-s">
+                                    <div>
+                                        <img src="./img/icon/Isaac Oluwatemilorun.svg" alt="img" />
+                                    </div>
+                                    <div class="info">
+                                        <p class="sec-car-name">Isaac Oluwatemilorun</p>
+                                        <p><img class="flag" src="./img/icon/it.svg" alt="icon flag" /><span class="country">Palo Alto, CA</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="info-content">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae in donec in nisi vitae.
+                                    Vestibulum pellentesque eget
+                                    laoreet adipiscing.
+                                    </div>
+                            </div>
+                        </div>
+
+                        <div class="second-card-holder">
+                            <div class="second-card-holder-in">
+                                <div class="sec-car-h-s">
+                                    <div>
+                                        <img src="./img/icon/Img (1).svg" alt="icon-man" />
+                                    </div>
+                                    <div class="info">
+                                        <p class="sec-car-name">Hellen Jummy</p>
+                                        <p><img class="flag" src="./img/icon/us.svg" alt="icon-flag" /><span class="country">Palo Alto, CA</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="info-content">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae in donec in nisi vitae.
+                                    Vestibulum pellentesque eget
+                                    laoreet adipiscing.
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Home;
